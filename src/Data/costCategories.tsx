056c4ea32e91e4c9d3c754be1f87c1bdc0c192a4
matthewjburton <mattjburton64@gmail.com>
2024-08-15 10:05:14 -0400

    date selection, cost category selection, and cost code input/display for appropriate categories

---
+++ src/Data/costCategories.tsx
@@ -0,0 +1,12 @@
+export const costCategories = [
+  { category: "Air Fare", costCode: "62-1001-TRV" },
+  { category: "Car Rental", costCode: "62-1004-TRV" },
+  { category: "Reimbursable Gas", costCode: "62-1011-TRV" },
+  { category: "Hotel", costCode: "62-1002-TRV" },
+  { category: "Tolls/Parking", costCode: "62-1009-TRV" },
+  { category: "Client Entertainment", costCode: "62-1006-MLJ" },
+  { category: "Per Diem", costCode: "62-1005-MLJ" },
+  { category: "Job Site Material", costCode: "" },
+  { category: "Milage", costCode: "" },
+  { category: "Other", costCode: "" },
+];
