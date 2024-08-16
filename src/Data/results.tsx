2b2890f4c01684a77c07da83be165fee7511f387
tanujk-ATEC <tanujk@advancetecllc.com>
2024-08-16 11:26:52 -0400

    PDF caption template + Begin calculations + summary work
    pdf caption template on each attachment, working on the logic for each expense type

---
+++ src/Data/results.tsx
@@ -0,0 +1,16 @@
+export const total = { value: 0 };
+export const totalTaxed = { value: 0 };
+export const totalUntaxed = { value: 0 };
+export const breakdown = [
+  { category: "Air Fare", sum: 0 },
+  { category: "Car Rental", sum: 0 },
+  { category: "Reimbursable Gas", sum: 0 },
+  { category: "Hotel", sum: 0 },
+  { category: "Tolls/Parking", sum: 0 },
+  { category: "Client Entertainment", sum: 0 },
+  { category: "Per Diem", sum: 0 },
+  { category: "Job Site Material", sum: 0 },
+  { category: "Milage", sum: 0 },
+  { category: "Other", sum: 0 },
+];
+export const summaries = [];
