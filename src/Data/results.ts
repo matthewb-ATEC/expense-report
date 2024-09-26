65ba5bae7194c45cddfec75b04a017f35e9da268
tanujk-ATEC <tanujk@advancetecllc.com>
2024-09-26 16:45:11 -0400

    Final PDF
    Made substantial changes to the PDF and calculations logic.

--- src/Data/results.ts
+++ src/Data/results.ts
@@ -6,13 +6,13 @@ export const total = { value: 0 };
 export const totalTaxed = { value: 0 };
 export const totalUntaxed = { value: 0 };
 export const breakdown = [
-  { category: "Air Fare", sum: 0 },
-  { category: "Car Rental", sum: 0 },
-  { category: "Reimbursable Gas", sum: 0 },
-  { category: "Hotel", sum: 0 },
-  { category: "Tolls/Parking", sum: 0 },
-  { category: "Client Entertainment", sum: 0 },
-  { category: "Per Diem", sum: 0 },
+  { category: "62-1001-TRV - Air Fare", sum: 0 },
+  { category: "62-1004-TRV - Car Rental", sum: 0 },
+  { category: "62-1011-TRV - Reimbursable Gas", sum: 0 },
+  { category: "62-1002-TRV - Hotel", sum: 0 },
+  { category: "62-1009-TRV - Tolls/Parking", sum: 0 },
+  { category: "62-1006-MLJ - Client Entertainment", sum: 0 },
+  { category: "62-1005-MLJ - Per Diem", sum: 0 },
   { category: "Job Site Material", sum: 0 },
   { category: "Mileage", sum: 0 },
   { category: "Other", sum: 0 },
