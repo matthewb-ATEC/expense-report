cf3996b52a43dbe491c1d645eeda94dbd54710bd
matthewjburton <mattjburton64@gmail.com>
2024-09-27 13:59:57 -0400

    preliminary backend

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,27 @@
-const listen = require("./app");
-const PORT = require("./utils/config");
-const info = require("./utils/logger");
+const express = require("express");
+const app = express();
+const cors = require("cors");
 
-listen(PORT, () => {
-  info(`Server running on port ${PORT}`);
+app.use(cors());
+app.use(express.static("dist"));
+
+let projects = [];
+
+app.get("/", (request, response) => {
+  response.send("<h1>Hello World!</h1>");
+});
+
+app.get("/projects", (request, response) => {
+  response.json(projects);
+});
+
+app.put("/projects", (request, response) => {
+  const updatedProjects = request.body;
+  projects = updatedProjects;
+  response.json(projects);
+});
+
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
