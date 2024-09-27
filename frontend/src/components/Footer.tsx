350e35cf74a73f0f11ca45d50e87cd40c5008ca2
matthewjburton <mattjburton64@gmail.com>
2024-09-27 11:30:14 -0400

    vite frontend

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Footer: React.FC = () => {
   return (
-    <div className="text-center p-8 text-white">
+    <div className="text-center p-8 text-white bg-ATECblue">
       Contact matthewb@advancetecllc.com or tanujk@advancetecllc.com for support
     </div>
   );
