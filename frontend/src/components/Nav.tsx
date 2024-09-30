9d387d6a8f35a63f4a498d22cc443ef10a80cd7e
matthewjburton <mattjburton64@gmail.com>
2024-09-30 14:48:13 -0400

    start of settings config and routes

---
+++ frontend/src/components/Nav.tsx
@@ -0,0 +1,10 @@
+const Nav = () => {
+  return (
+    <div className="p-4 w-full flex space-x-8 justify-end bg-ATECblue text-white">
+      <div>Expense Report</div>
+      <div>Settings</div>
+    </div>
+  );
+};
+
+export default Nav;
