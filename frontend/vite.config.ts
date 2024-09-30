93951816bedb8566b393a0333365b0c0219134de
matthewjburton <mattjburton64@gmail.com>
2024-09-30 10:44:29 -0400

    maps search

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      "/": {
+      "/projects": {
         target: "http://localhost:3001",
         changeOrigin: true,
       },
