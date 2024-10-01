ce2af6ad7ebe0ad66f3b4b8f29d5aa1761aab6e2
matthewjburton <mattjburton64@gmail.com>
2024-10-01 09:52:33 -0400

    header text updates on route changes

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,6 +1,21 @@
 import React from "react";
+import { useLocation } from "react-router-dom";
+
+const headers: { [key: string]: string } = {
+  "/": "Expense Report",
+  "/report": "Expense Report",
+  "/settings": "Settings",
+};
+
+const subheaders: { [key: string]: string } = {
+  "/": "File an expense report using the form below.",
+  "/report": "File an expense report using the form below.",
+  "/settings": "View and edit the parameters underlying expense calculations.",
+};
 
 const Header: React.FC = () => {
+  const location = useLocation();
+
   return (
     <div className="flex flex-col space-y-4 w-full items-center justify-center p-8 bg-white shadow-lg">
       <img
@@ -8,7 +23,14 @@ const Header: React.FC = () => {
         src="/images/ATEC Pantone Colors-hort.png"
         alt="AdvanceTEC"
       />
-      <div className="text-2xl font-semibold">Expense Report Form</div>
+      <div className="flex flex-col text-center space-y-2">
+        <div className="text-2xl font-semibold">
+          {headers[location.pathname]}
+        </div>
+        <div className="text-sm text-gray-500">
+          {subheaders[location.pathname]}
+        </div>
+      </div>
     </div>
   );
 };
