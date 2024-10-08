6d71a7675a4f8e7193f18ce787b43791865c4122
tanujk-ATEC <tanujk@advancetecllc.com>
2024-10-08 15:26:03 -0400

    New categories + Bug fixing

--- frontend/src/data/results.ts
+++ frontend/src/data/results.ts
@@ -19,14 +19,19 @@
 export const total = { value: 0 };
 export const breakdown = [
   { category: "Air Fare", costCode: "62-1001-TRV", sum: 0 },
+  { category: "Apartments", costCode: "62-1003-TRV", sum: 0 },
   { category: "Car Rental", costCode: "62-1004-TRV", sum: 0 },
-  { category: "Reimbursable Gas", costCode: "62-1011-TRV", sum: 0 },
-  { category: "Hotel", costCode: "62-1002-TRV", sum: 0 },
-  { category: "Tolls/Parking", costCode: "62-1009-TRV", sum: 0 },
   { category: "Client Entertainment", costCode: "62-1006-TRV", sum: 0 },
+  { category: "Company Events", costCode: "62-1007-TRV", sum: 0 },
+  { category: "Hotel", costCode: "62-1002-TRV", sum: 0 },
+  { category: "Job Site Material", costCode: "01-7200-MAT", sum: 0 },
+  { category: "Mileage", costCode: "62-1009-TRV", sum: 0 },
+  { category: "Parking", costCode: "62-1010-TRV", sum: 0 },
   { category: "Per Diem", costCode: "62-1005-TRV", sum: 0 },
-  { category: "Job Site Material", costCode: "", sum: 0 },
-  { category: "Mileage", costCode: "", sum: 0 },
+  { category: "Reimbursable Gas", costCode: "62-1011-TRV", sum: 0 },
+  { category: "Relocation", costCode: "62-1008-TRV", sum: 0 },
+  { category: "Tolls", costCode: "62-1009-TRV", sum: 0 },
+  { category: "Wellness", costCode: "64-6161-OVH", sum: 0 },
   { category: "Other", costCode: "", sum: 0 },
 ];
 export const summaries = [];
