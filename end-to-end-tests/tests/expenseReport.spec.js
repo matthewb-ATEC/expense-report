0c7654a47c384768816706d34a7fc9e840068b99
matthewb-ATEC <matthewb@advancetecllc.com>
2024-11-08 16:52:20 -0500

    fixed basic end to end tests

--- end-to-end-tests/tests/expenseReport.spec.js
+++ end-to-end-tests/tests/expenseReport.spec.js
@@ -1,16 +1,10 @@
-import { test, expect } from '@playwright/test'
-import { beforeEach, describe } from 'node:test'
+import { test, expect, beforeEach, describe } from '@playwright/test'
 import { startReport } from './helper'
 
 describe('Expense Report app', () => {
   beforeEach(async ({ page, request }) => {
     await request.post('/api/testing/reset')
     await page.goto('/')
-  })
-
-  test('Expense report button is shown', async ({ page }) => {
-    const expenseReportButton = page.getByText('Expense Report')
-    await expect(expenseReportButton).toBeVisible()
   })
 
   test('Settings button is shown', async ({ page }) => {
@@ -20,7 +14,7 @@ describe('Expense Report app', () => {
 
   test('a new report can be created', async ({ page }) => {
     await startReport(page)
-    const projectsHeader = page.getByText('Projects')
+    const projectsHeader = page.getByText('Project').last()
     await expect(projectsHeader).toBeVisible()
   })
 })
