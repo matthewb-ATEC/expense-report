935bfe12ca4757ad7219703cb9368ab413ac48ad
matthewjburton <mattjburton64@gmail.com>
2024-11-18 15:48:35 -0500

    new pdf image

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -13,31 +13,27 @@
  * @relatedFiles Related components include `Footer.tsx`, `ExpenseReport.tsx`, and any other components that make up the main layout of the application.
  */
 
-import React from "react";
-import { useLocation } from "react-router-dom";
+import React from 'react'
+import { useLocation } from 'react-router-dom'
 
 const headers: Record<string, string> = {
-  "/": "Expense Report",
-  "/report": "Expense Report",
-  "/settings": "Settings",
-};
+  '/': 'Expense Report',
+  '/report': 'Expense Report',
+  '/settings': 'Settings',
+}
 
 const subheaders: Record<string, string> = {
-  "/": "File an expense report using the form below.",
-  "/report": "File an expense report using the form below.",
-  "/settings": "View and edit the parameters underlying expense calculations.",
-};
+  '/': 'File an expense report using the form below.',
+  '/report': 'File an expense report using the form below.',
+  '/settings': 'View and edit the parameters underlying expense calculations.',
+}
 
 const Header: React.FC = () => {
-  const location = useLocation();
+  const location = useLocation()
 
   return (
     <div className="flex flex-col space-y-4 w-full items-center justify-center p-8 bg-white shadow-lg">
-      <img
-        className="h-16"
-        src="/images/ATEC Pantone Colors-hort.png"
-        alt="AdvanceTEC"
-      />
+      <img className="h-16" src="/images/ATEC-logo.png" alt="AdvanceTEC" />
       <div className="flex flex-col text-center space-y-2">
         <div className="text-2xl font-semibold">
           {headers[location.pathname]}
@@ -47,7 +43,7 @@ const Header: React.FC = () => {
         </div>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default Header;
+export default Header
